9e3eaaff48265aeead47b1ac4025b03ec92e7a2e
Jai <malhi.jaivir@gmail.com>
2026-02-28 18:38:28 -0800

    Fix title not being centered on home and about page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -26,67 +26,65 @@ import Revolve from "$components/pages/Spotlight/Revolve/Revolve";
 
 export default function Home() {
    return (
-      <>
-         <Container>
-            <Title
-               Header="Personal Growth,"
-               Header2="Community Impact"
-               Body="Axis Consulting is a student-led pro-bono consultancy part of Simon
+      <Container>
+         <Title
+            Header="Personal Growth,"
+            Header2="Community Impact"
+            Body="Axis Consulting is a student-led pro-bono consultancy part of Simon
             Fraser University"
-            />
+         />
 
-            <StatsGrid>
-               <StatColumn>
-                  <Icon Icon={IoIosCheckmarkCircleOutline} Text="70+ Projects" sizePercent={80} />
-                  <Icon Icon={MdOutlinePeopleAlt} Text="268 Alumni" />
-                  <Icon Icon={IoMdStarOutline} Text="4.5/5 Rating" />
-               </StatColumn>
+         <StatsGrid>
+            <StatColumn>
+               <Icon Icon={IoIosCheckmarkCircleOutline} Text="70+ Projects" sizePercent={80} />
+               <Icon Icon={MdOutlinePeopleAlt} Text="268 Alumni" />
+               <Icon Icon={IoMdStarOutline} Text="4.5/5 Rating" />
+            </StatColumn>
 
-               <StatColumn>
-                  <Icon Icon={BsPaperclip} Text="200+ Students Engaged" />
-                  <Icon Icon={FaRegHeart} Text="96% Satisfaction" />
-                  <Icon Icon={FaRegStickyNote} Text="98% Implementation" />
-               </StatColumn>
-            </StatsGrid>
+            <StatColumn>
+               <Icon Icon={BsPaperclip} Text="200+ Students Engaged" />
+               <Icon Icon={FaRegHeart} Text="96% Satisfaction" />
+               <Icon Icon={FaRegStickyNote} Text="98% Implementation" />
+            </StatColumn>
+         </StatsGrid>
 
-            {/* Update Spotlight Section as needed */}
-            <SpotlightSection>
-               <h3>Spotlight</h3>
-               {/* <Odyssey isLargeCardVisible={false} /> */}
-               <Revolve />
-            </SpotlightSection>
+         {/* Update Spotlight Section as needed */}
+         <SpotlightSection>
+            <h3>Spotlight</h3>
+            {/* <Odyssey isLargeCardVisible={false} /> */}
+            <Revolve />
+         </SpotlightSection>
 
-            <CenterStack>
-               <h1>Our Engagements</h1>
-               <EngagementRow>
-                  <ImageContainer
-                     Header="Visier"
-                     Body="Market research on the field of HR analytics, market trends in HR and finance interactions"
-                     CTA="More information on the client"
-                     Image={HOME_URLS.visier_card}
-                     clickTo={CLIENT_LOGOS_BY_KEY.visier.website}
-                     loading="lazy"
-                  />
-                  <ImageContainer
-                     Header="West Coast Kids Cancer Foundation"
-                     Body="Analysis on the supply chain, bottleneck identification, and recommendation of a just-in-time inventory system"
-                     CTA="More information on the client"
-                     Image={HOME_URLS.wck_card}
-                     clickTo={CLIENT_LOGOS_BY_KEY.wck.website}
-                     loading="lazy"
-                  />
-               </EngagementRow>
-            </CenterStack>
+         <CenterStack>
+            <h1>Our Engagements</h1>
+            <EngagementRow>
+               <ImageContainer
+                  Header="Visier"
+                  Body="Market research on the field of HR analytics, market trends in HR and finance interactions"
+                  CTA="More information on the client"
+                  Image={HOME_URLS.visier_card}
+                  clickTo={CLIENT_LOGOS_BY_KEY.visier.website}
+                  loading="lazy"
+               />
+               <ImageContainer
+                  Header="West Coast Kids Cancer Foundation"
+                  Body="Analysis on the supply chain, bottleneck identification, and recommendation of a just-in-time inventory system"
+                  CTA="More information on the client"
+                  Image={HOME_URLS.wck_card}
+                  clickTo={CLIENT_LOGOS_BY_KEY.wck.website}
+                  loading="lazy"
+               />
+            </EngagementRow>
+         </CenterStack>
 
-            <ImageContent
-               Header="Ready to Soar?"
-               Body="Join the team, come to events, and expand horizons within the field of consulting"
-               ButtonText="Get Involved"
-               ImageSrc={HOME_URLS.get_involved_card}
-               AltText="The Axis Consulting Organization 2024-2025 posing for our annual group photo"
-               clickTo={ROUTES.GET_INVOLVED}
-            />
-         </Container>
-      </>
+         <ImageContent
+            Header="Ready to Soar?"
+            Body="Join the team, come to events, and expand horizons within the field of consulting"
+            ButtonText="Get Involved"
+            ImageSrc={HOME_URLS.get_involved_card}
+            AltText="The Axis Consulting Organization 2024-2025 posing for our annual group photo"
+            clickTo={ROUTES.GET_INVOLVED}
+         />
+      </Container>
    );
 }
