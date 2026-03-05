b3ac1ade48a898d9176aa8a70c2a21a6e6437b14
Jai <malhi.jaivir@gmail.com>
2026-03-05 14:07:33 -0800

    Sponsors: add additional sponsors marquee for in-kind sponsors

--- src/constants/pages/sponsors.ts
+++ src/constants/pages/sponsors.ts
@@ -1,31 +1,96 @@
 // src/constants/clients.ts
-const SPONSOR_BUCKET = "sponsors-page/";
 
 // URLs for all home page image assets using R2 path
 export const SPONSOR_URL = {
    work_with_us: "generic/operations-all.webp",
 } as const;
 
-export const SPONSOR_LOGOS = [
+// Sponsor companies (for logo marquee)
+const SPONSORS_COMPANY_BUCKET = "sponsors-page/";
+
+export const SPONSORS_COMPANY_LOGOS = [
    {
-      key: "deloitte",
+      key: "deloitte", // monetary sponsor
       name: "Deloitte",
-      logoSrc: `${SPONSOR_BUCKET}deloitte.webp`,
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}deloitte.webp`,
+      website: "https://www.deloitte.com/",
    },
    {
-      key: "ey",
-      name: "Ernst & Young (EY)",
-      logoSrc: `${SPONSOR_BUCKET}ey.webp`,
+      key: "ey", // monetary sponsor
+      name: "EY",
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}ey.webp`,
+      website: "https://www.ey.com/",
+   },
+   {
+      key: "black_bond_books",
+      name: "Black Bond Books",
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}Black-Bond-Books.webp`,
+      website: "https://www.blackbondbooks.com/",
+   },
+   {
+      key: "bon_macaron",
+      name: "Bon Macaron",
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}Bon-Macaron.webp`,
+      website: "https://www.bonmacaronpatisserie.com/",
+   },
+   {
+      key: "cinnzeo",
+      name: "Cinnzeo",
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}Cinnzeo.webp`,
+      website: "https://www.cinnzeo.com/",
+   },
+   {
+      key: "forbidden_vancouver",
+      name: "Forbidden Vancouver",
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}Forbidden-Vancouver.webp`,
+      website: "https://forbiddenvancouver.ca/",
+   },
+   {
+      key: "goodbowl",
+      name: "Goodbowl",
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}Goodbowl.webp`,
+      website: "https://www.goodbowl.ca/",
+   },
+   {
+      key: "kernels",
+      name: "Kernels Popcorn",
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}Kernels.webp`,
+      website: "https://www.kernelspopcorn.com/",
+   },
+   {
+      key: "old_spaghetti_factory",
+      name: "Old Spaghetti Factory",
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}Old-Spaghetti-Factory.webp`,
+      website: "https://oldspaghettifactory.ca/",
+   },
+   {
+      key: "sfu_bookstore",
+      name: "SFU Bookstore",
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}SFU-Bookstore.webp`,
+      website: "https://shop.sfu.ca/",
+   },
+   {
+      key: "vancouver_art_gallery",
+      name: "Vancouver Art Gallery",
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}Vancouver-Art-Gallery.webp`,
+      website: "https://www.vanartgallery.bc.ca/",
+   },
+   {
+      key: "viff",
+      name: "Vancouver International Film Festival",
+      logoSrc: `${SPONSORS_COMPANY_BUCKET}VIFF.webp`,
+      website: "https://viff.org/",
    },
 ] as const;
 
-export type SponsorLogo = (typeof SPONSOR_LOGOS)[number];
-export type SponsorKey = SponsorLogo["key"];
+export type SponsorsCompanyLogo = (typeof SPONSORS_COMPANY_LOGOS)[number];
+export type SponsorsCompanyKey = SponsorsCompanyLogo["key"];
 
-export const SPONSOR_LOGOS_BY_KEY: Record<SponsorKey, SponsorLogo> = SPONSOR_LOGOS.reduce(
-   (acc, sponsor) => {
-      acc[sponsor.key] = sponsor;
-      return acc;
-   },
-   {} as Record<SponsorKey, SponsorLogo>
-);
+export const SPONSORS_COMPANY_LOGOS_BY_KEY: Record<SponsorsCompanyKey, SponsorsCompanyLogo> =
+   SPONSORS_COMPANY_LOGOS.reduce(
+      (acc, company) => {
+         acc[company.key] = company;
+         return acc;
+      },
+      {} as Record<SponsorsCompanyKey, SponsorsCompanyLogo>,
+   );
